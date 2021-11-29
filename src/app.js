443b4fc03aa1b561fef051c4c94c610a954154fe
wpaq <wallysonpabloo@gmail.com>
2021-11-29 18:53:25 -0300

    Adicionando cors

--- src/app.js
+++ src/app.js
@@ -6,11 +6,26 @@ import { resolve } from 'path';
 import './database';
 
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+
 import homeRoutes from './routes/homeRoutes';
 import userRoutes from './routes/userRoutes';
 import alunoRoutes from './routes/alunoRoutes';
 import tokenRoutes from './routes/tokenRoutes';
 import photoRoutes from './routes/photoRoutes';
+
+const whiteList = ['http://localhost:3000'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whiteList.indexOf(origin) != -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 class App {
   constructor() {
@@ -20,6 +35,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors());
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, 'uploud')));
--- src/controllers/TokenController.js
+++ src/controllers/TokenController.js
@@ -30,7 +30,7 @@ class TokenController {
       expiresIn: process.env.TOKEN_EXPIRATION,
     });
 
-    res.json({ token });
+    res.json({ token, user: user.nome, id, email });
   }
 }
 
